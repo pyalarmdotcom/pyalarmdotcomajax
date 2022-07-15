93eef5fe6ae42d61821f484fe4d267be567805b3
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2022-07-15 08:19:23 -0400

    Update __init__.py

--- pyalarmdotcomajax/__init__.py
+++ pyalarmdotcomajax/__init__.py
@@ -37,7 +37,7 @@ from .extensions import CameraSkybellControllerExtension
 from .extensions import ConfigurationOption
 from .extensions import ExtendedProperties
 
-__version__ = "0.3.4"
+__version__ = "0.4"
 
 
 log = logging.getLogger(__name__)
