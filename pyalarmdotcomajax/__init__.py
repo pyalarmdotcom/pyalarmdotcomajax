dad3a1d0a2543597d64e1cbb6d9f5f43af623314
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2022-12-14 23:37:01 +0000

    Added output from actual gate to tests.

--- pyalarmdotcomajax/__init__.py
+++ pyalarmdotcomajax/__init__.py
@@ -38,7 +38,7 @@ from .extensions import CameraSkybellControllerExtension
 from .extensions import ConfigurationOption
 from .extensions import ExtendedProperties
 
-__version__ = "0.4.8"
+__version__ = "0.4.8-beta"
 
 log = logging.getLogger(__name__)
 
