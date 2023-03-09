28372c00e654974abb9e1aa64611ad8eb295b5a7
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2023-03-09 02:02:09 +0000

    Pre-commit / devcontainer changes.

---
+++ scripts/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pip install -r requirements-dev.txt
+pre-commit install
+pre-commit install-hooks
+pip install --editable .
--- setup.py
+++
@@ -1,8 +0,0 @@
-"""Stub for legacy builds that allows pip to install an editable version of this module."""
-
-# File Version: 2023.01.27
-
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup()
