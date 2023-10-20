bfa9f058c507560b455fe4e7a2acc69843dd0d96
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2023-10-20 20:09:32 +0000

    Bump version number.

--- pyalarmdotcomajax/__init__.py
+++ pyalarmdotcomajax/__init__.py
@@ -46,7 +46,7 @@ from pyalarmdotcomajax.extensions import (
 )
 from pyalarmdotcomajax.websockets.client import WebSocketClient, WebSocketState
 
-__version__ = "0.5.5-dev.1"
+__version__ = "0.5.5"
 
 log = logging.getLogger(__name__)
 
