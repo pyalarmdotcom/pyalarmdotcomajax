a53fc7e5fb53a57e878335e27516403b81df177d
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2023-10-27 15:40:40 +0000

    Added scenes to tests.

--- tests/responses/__init__.py
+++ tests/responses/__init__.py
@@ -6,10 +6,10 @@ from importlib import resources
 def get_http_body_json(name: str) -> str:
     """Get server/client response/request body from JSON file."""
 
-    return resources.read_text(__package__, f"{name}.json")
+    return resources.files(__package__).joinpath(f"{name}.json").read_text()
 
 
 def get_http_body_html(name: str) -> str:
     """Get server/client response/request body from HTML file."""
 
-    return resources.read_text(__package__, f"{name}.html")
+    return resources.files(__package__).joinpath(f"{name}.html").read_text()
