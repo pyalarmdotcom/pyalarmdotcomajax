f9fc062d935adbc8c549dd77513a7afe5ca64374
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2024-04-06 00:35:43 +0000

    Fixed log format

--- pyalarmdotcomajax/adc/common.py
+++ pyalarmdotcomajax/adc/common.py
@@ -34,7 +34,6 @@ MAIN_OUTPUT = "Output Flags"
 CREDENTIALS = "User Credentials"
 
 logging.basicConfig(
-    format="%(asctime)s %(levelname)-8s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[RichHandler(rich_tracebacks=True)],
 )
